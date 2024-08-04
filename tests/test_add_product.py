c9f17d60b81a16175042d0903ead31236bf3f240
ChesterHsieh <chsieh5@hawk.iit.edu>
2024-08-04 15:38:04 +0800

    [TEST] use pytest instead

--- tests/test_add_product.py
+++ tests/test_add_product.py
@@ -1,23 +1,44 @@
-import unittest
-from src.core.use_cases.add_product import AddProduct
-from unittest.mock import Mock
+import pytest
 
-class TestAddProduct(unittest.TestCase):
-    def setUp(self):
-        self.product_repository = Mock()
-        self.add_product_use_case = AddProduct(self.product_repository)
+from src.core.entities.order import Order
+from src.core.use_cases.add_order import AddOrder
+from unittest.mock import Mock, MagicMock
 
-    def test_add_product_successfully(self):
-        product_data = {
-            "id": "1",
-            "name": "Test Product",
-            "description": "Test Description",
-            "price": 10.0,
-            "inventory_quantity": 100
-        }
+from src.infrastructure.dao.order_dao import OrderDAO
 
-        self.add_product_use_case.execute(product_data)
-        self.product_repository.add_product.assert_called_once()
 
-if __name__ == '__main__':
-    unittest.main()
+# Follow DIP principle, the test become super easy to write
+# Mocking the repository, we could have
+# 1) called test
+# 2) mock object return value
+
+
+@pytest.fixture
+def order_dao():
+    return MagicMock(spec=OrderDAO)
+
+@pytest.fixture
+def add_order_use_case(order_dao):
+    return AddOrder(order_dao)
+
+def adds_order_successfully(add_order_use_case, order_dao):
+    order_data = {
+        "id": "1",
+        "user_id": "1",
+        "total_amount": 1500.00,
+        "status": "pending"
+    }
+    order = Order(**order_data)
+    add_order_use_case.execute(order)
+    order_dao.add_order.assert_called_once_with(order)
+
+def raises_exception_for_invalid_order(add_order_use_case):
+    order_data = {
+        "id": "1",
+        "user_id": "1",
+        "total_amount": -1500.00,  # Invalid total amount
+        "status": "pending"
+    }
+    order = Order(**order_data)
+    with pytest.raises(ValueError):
+        add_order_use_case.execute(order)
